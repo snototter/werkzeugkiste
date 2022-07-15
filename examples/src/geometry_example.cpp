93eee5f4c8ae13db499603074d9270ee27a3a837
snototter <snototter@users.noreply.github.com>
2022-07-15 17:04:14 +0200

    transformations & projections

--- examples/src/geometry_example.cpp
+++ examples/src/geometry_example.cpp
@@ -1,14 +1,62 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <ostream>
 #include <utility>
+#include <initializer_list>
 
 // Only needed to query the library version:
 #include <werkzeugkiste/version.h>
 
 #include <werkzeugkiste/geometry/vector.h>
 #include <werkzeugkiste/geometry/projection.h>
+
+
+//template <typename _V>
+//std::string PrettyPrint(const _V &vec) {
+//  std::ostringstream s;
+//  for (int i = 0; i < _V::ndim; ++i) {
+//    if (i > 0) {
+//      s << "\n";
+//    }
+//    s << vec[i];
+//  }
+//  return s.str();
+//}
+
+
+template <typename _V>
+std::string PrettyPrint(
+    std::initializer_list<_V> vecs,
+    std::size_t indent_first_row = 0,
+    std::size_t indent_others = 0) {
+  std::ostringstream s;
+  s << "[ ";
+
+  const auto lst_it = vecs.begin();
+  for (int dim = 0; dim < _V::ndim; ++dim) {
+    if (dim > 0) {
+      s << "\n";
+      for (std::size_t idx = 0; idx < indent_others + 2; ++idx) {
+        s << ' ';
+      }
+    } else {
+      for (std::size_t idx = 0; idx < indent_first_row; ++idx) {
+        s << ' ';
+      }
+    }
+
+    for (std::size_t idx = 0; idx < vecs.size(); ++idx) {
+      s << std::fixed << std::setw(10) << std::setprecision(3) << lst_it[idx][dim];
+      if (idx < vecs.size() - 1) {
+        s << ", ";
+      }
+    }
+  }
+  s << " ]";
+  return s.str();
+}
 
 
 int main(int /* argc */, char ** /* argv */) {
@@ -20,28 +68,55 @@ int main(int /* argc */, char ** /* argv */) {
             << std::endl;
 
   //TODO
-  auto v1 = wkg::Vec2d{17, 42};
+  wkg::Vec2d v1{17, 42}, v2{9, -3}, v3{0, 0.01};
 
   std::cout << "Vector:      " << v1
           << "\n--> Length:  " << v1.Length()
           << "\nUnit vector: " << v1.UnitVector()
           << "\n--> Length:  " << v1.UnitVector().Length() << std::endl;
 
-  auto v2 = wkg::Vec2i{3, 9};
-  std::cout << "Conversion: " << wkg::VecToEigen(v1)
-            << "\nHomogeneous: " << wkg::VecToEigenHomogeneous(v1)
-            << "\nMat(3 vecs): " << wkg::VecsToEigen(v1, v1, v1)
-            << std::endl;
+//  auto v2 = wkg::Vec2i{3, 9};
+//  std::cout << "Conversion: " << wkg::VecToEigenCol(v1)
+//            << "\nHomogeneous: " << wkg::VecToEigenHomogeneous(v1)
+//            << "\nMat(3 vecs): " << wkg::VecsToEigenMat(v1, v1, v1)
+//            << std::endl;
 
-  std::cout << "Conversion: " << wkg::VecToEigen(v2)
-            << "\nHomogeneous: " << wkg::VecToEigenHomogeneous(v2)
-            << "\nMat(3 vecs): " << wkg::VecsToEigen(v1, static_cast<wkg::Vec2d>(v2), v1)
-            << std::endl;
+//  std::cout << "Conversion: " << wkg::VecToEigenCol(v2)
+//            << "\nHomogeneous: " << wkg::VecToEigenHomogeneous(v2)
+//            << "\nMat(3 vecs): " << wkg::VecsToEigenMat(v1, static_cast<wkg::Vec2d>(v2), v1)
+//            << std::endl;
 
-  wkg::Vec2d a, b;
-  auto trafo = wkg::VecsToEigen(v1, static_cast<wkg::Vec2d>(v2), v1);
-  std::tie(a, b) = wkg::TransformVecs(trafo, v1, v1);
-  //std::tie(a,b);
+  wkg::Matrix<double, 4, 2> M;
+  M << 1, 2, 3, 4,
+       5, 6, 7, 8;
+  wkg::Matrix<double, 4, 3> mat_res = wkg::TransformToMat(M, v1, v2, v3);
+
+  wkg::Vec4d a, b, c;
+  std::tie(a, b, c) = wkg::EigenMatToVecTuple(mat_res);//, std::make_index_sequence<3>{});
+  std::cout << "Transformation v1:\n" << M << " * " << PrettyPrint({v1, v2, v3}, 0, 6)
+            << " =\n" << PrettyPrint({a, b, c}) << std::endl;
+
+  std::tie(a, b, c) = wkg::TransformToVecs(M, v1, v2, v3);
+  std::cout << "Transformation v2:\n" << M << " * " << PrettyPrint({v1, v2, v3}, 0, 6)
+            << " =\n" << PrettyPrint({a, b, c}) << std::endl;
+
+  std::tie(c) = wkg::TransformToVecs(M, v2);
+  std::cout << "Transformation single vec:\n" << M << " * " << PrettyPrint({v2}, 0, 6)
+            << " =\n" << PrettyPrint({c}) << std::endl;
+
+  wkg::Matrix<double, 3, 3> P;
+  P << 1, 2, 3,
+       4, 5, 6,
+       7, 8, 9;
+  wkg::Vec2d p1, p2, p3;
+  std::tie(p1, p2) = wkg::ProjectInhomogeneousToVecs(P, v1, v2);
+
+  std::cout << "Projection:\n" << P << " * " << PrettyPrint({v1, v2}, 0, 8)
+            << " =\n" << PrettyPrint({p1, p2}) << std::endl;
+
+  std::tie(p3) = wkg::ProjectInhomogeneousToVecs(P, v3);
+  std::cout << "Projection single vec:\n" << P << " * " << PrettyPrint({v3}, 0, 8)
+            << " =\n" << PrettyPrint({p3}) << std::endl;
 
  return 0;
 }
--- include/werkzeugkiste/geometry/projection.h
+++ include/werkzeugkiste/geometry/projection.h
@@ -3,8 +3,11 @@
 
 #include <tuple>
 #include <utility>
+#include <type_traits>
 
 #include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/Eigen>
 
 #include <werkzeugkiste/geometry/vector.h>
 
@@ -13,43 +16,36 @@ namespace werkzeugkiste {
 namespace geometry {
 
 template <typename _Tp, int Rows, int Columns>
-using MatrixBase = Eigen::Matrix<_Tp, Rows, Columns, Eigen::RowMajor>;
+using Matrix = Eigen::Matrix<_Tp, Rows, Columns, (Columns > 1) ? Eigen::RowMajor : 0>;
 
 template <typename _Tp, int Rows>
-using MatrixBaseDynWidth = Eigen::Matrix<_Tp, Rows, Eigen::Dynamic, Eigen::RowMajor>;
+using MatrixDynWidth = Eigen::Matrix<_Tp, Rows, Eigen::Dynamic, Eigen::RowMajor>;
 
 // row-major layout is not supported for vectors in eigen (and being a vector, the layout doesn't matter anyhow)
 template <typename _Tp, int Dimension>
 using VectorBase = Eigen::Matrix<_Tp, Dimension, 1>;
 
-//using Matrix3x3 = MatrixBase<double, 3, 3>;
-//using Matrix3x4 = MatrixBase<double, 3, 4>;
+using Mat2x2d = Matrix<double, 2, 2>;
+using Mat3x3d = Matrix<double, 3, 3>;
+using Mat3x4d = Matrix<double, 3, 4>;
 
-//template <int Rows, int Columns> inline
-//Vec<double, Dimension> Transform(const MatrixBase<double, )
 
-template <typename _Tp, int dim> inline
-VectorBase<_Tp, dim> VecToEigen(const Vec<_Tp, dim> &vec) {
-  VectorBase<_Tp, dim> m;
-  for (int i = 0; i < dim; ++i) {
-    m[i] = vec[i];
+/// Converts a werkzeugkiste vector to an Eigen vector (single-column matrix).
+template <typename _V> inline
+VectorBase<typename _V::value_type, _V::ndim>
+VecToEigenCol(const _V &vec) {
+  VectorBase<typename _V::value_type, _V::ndim> mat;
+  for (int i = 0; i < _V::ndim; ++i) {
+    mat[i] = vec[i];
   }
-  return m;
+  return mat;
 }
 
 
+/// Converts a werkzeugkiste vector to an Eigen vector (i.e. single-column
+/// matrix) and adds a homogeneous coordinate (set to 1).
 template <typename _Tp, int dim> inline
-Vec<_Tp, dim> EigenToVec(const VectorBase<_Tp, dim> &vec) {
-  Vec<_Tp, dim> v;
-  for (int i = 0; i < dim; ++i) {
-    v[i] = vec[i];
-  }
-  return v;
-}
-
-
-template <typename _Tp, int dim> inline
-VectorBase<_Tp, dim + 1> VecToEigenHomogeneous(const Vec<_Tp, dim> &vec) {
+VectorBase<_Tp, dim + 1> VecToEigenColHomogeneous(const Vec<_Tp, dim> &vec) {
   VectorBase<_Tp, dim + 1> m;
   for (int i = 0; i < dim; ++i) {
     m[i] = vec[i];
@@ -59,65 +55,211 @@ VectorBase<_Tp, dim + 1> VecToEigenHomogeneous(const Vec<_Tp, dim> &vec) {
 }
 
 
-template <typename _Tp, int dim> inline
-Vec<_Tp, dim> Transform(const MatrixBase<_Tp, dim, dim> &M, const Vec<_Tp, dim> &vec) {
-  VectorBase<_Tp, dim> res = M * VecToEigen(vec);
-  return EigenToVec(res);
+/// Returns a vector for the given matrix column.
+template <typename _Tp, int Columns>
+_Tp EigenColToVec(
+    const Matrix<typename _Tp::value_type, _Tp::ndim, Columns> &eig, int col) {
+//    const VectorBase<typename _Tp::value_type, _Tp::ndim> &eig) {
+  _Tp v;
+  for (int i = 0; i < _Tp::ndim; ++i) {
+    v[i] = eig(i, col);
+  }
+  return v;
 }
 
 
-// TODO check https://www.fluentcpp.com/2021/04/30/how-to-implement-stdconjunction-and-stddisjunction-in-c11/
-//template <typename _Tp, typename... _Ts>
-//using SameType = std::enable_if_t<std::conjunction_v<std::is_same<_Tp, _Ts>...>>;
-// TODO check https://www.fluentcpp.com/2021/06/07/how-to-define-a-variadic-number-of-arguments-of-the-same-type-part-5/
-//template <typename _Tp, typename... _Ts, typename = SameType<_Tp, _Ts...>> inline
 
-  //TODO: could result in compiler issues with gcc < 8? https://gcc.gnu.org/bugzilla/show_bug.cgi?id=51155#c1
-template <typename _Tp, typename... _Ts> inline
-//MatrixBaseDynWidth<typename _Tp::value_type, _Tp::ndim>
-MatrixBase<typename _Tp::value_type, _Tp::ndim, 1 + sizeof...(_Ts)>
-VecsToEigen(const _Tp &vec0, const _Ts &... others) {
+/// Returns a matrix where each column holds one vector.
+/// Caution:
+///   Matrix size is fixed at compile time, thus it will be created
+///   on the stack. Should only be used for a maximum of up to 32 vectors.
+///   Refer to the Eigen3 docs for details:
+///   https://eigen.tuxfamily.org/dox/group__TutorialMatrixClass.html
+template <int Dim, typename _V, typename... _Vs> inline
+Matrix<typename _V::value_type, Dim, 1 + sizeof...(_Vs)>
+VecsToEigenMat(
+    const _V &vec0, const _Vs &... others) {
+  static_assert(
+    (Dim == _V::ndim) || (Dim == (_V::ndim + 1)),
+    "Invalid number of rows for the matrix - must be either the vector "
+    "dimension or 1 more (for homogeneous)!");
+
   const int num_vecs = 1 + sizeof...(others);
-  const _Tp vecs[num_vecs] {
-    vec0, static_cast<const _Tp &>(others)...};
+  static_assert(
+    num_vecs <= 32,
+    "TODO warning text: Fixed size matrix...");
 
-  MatrixBase<typename _Tp::value_type, _Tp::ndim, num_vecs> m;
-  for (int row = 0; row < _Tp::ndim; ++row) {
+  const _V vecs[num_vecs] {
+    vec0, static_cast<const _V &>(others)...};
+
+  Matrix<typename _V::value_type, Dim, num_vecs> m;
+  for (int row = 0; row < _V::ndim; ++row) {
     for (int col = 0; col < num_vecs; ++col) {
       m(row, col) = vecs[col][row];
+    }
+  }
+
+  if (Dim == _V::ndim + 1) {
+    // Add homogeneous coordinate:
+    for (int col = 0; col < num_vecs; ++col) {
+      m(_V::ndim, col) = static_cast<typename _V::value_type>(1);
     }
   }
   return m;
 }
 
 
-
-//TODO!!tuple cat https://stackoverflow.com/a/10015033
-template <typename _Tp>
-std::tuple<_Tp> VecsToTuple(const _Tp & vec) { //TODO
-  return std::tuple<_Tp>(vec);
+/// Base case for making a tuple out of a variadic number of vectors.
+template <typename _Tp> inline
+std::tuple<_Tp> VecsToTuple(_Tp &&vec) {
+  return std::tuple<_Tp>(std::move(vec));
 }
 
-template <typename _First, typename _Second, typename... _Rest>
-std::tuple<_First, _Second, _Rest...> VecsToTuple(const _First &v0, const _Second &v1, const _Rest&... others) {
-  //_First vec; //TODO
+
+/// Returns a tuple of vectors.
+template <typename _V0, typename _V1, typename... _Vs> inline
+std::tuple<_V0, _V1, _Vs...>
+VecsToTuple(
+    _V0 &&v0, _V1 &&v1, _Vs&&... others) {
   return std::tuple_cat(
-        std::tuple<_First>(v0),//std::tuple<_First>(std::move(vec)),
-        VecsToTuple<_Second, _Rest...>(v1, others...));//VecsToTuple<_Second, _Rest...>(std::move(v1), std::move(others)...));
+        std::tuple<_V0>(std::move(v0)),
+        VecsToTuple<_V1, _Vs...>(std::move(v1), std::move(others)...));
 }
 
-//todo likely needs smth like 'apply' https://stackoverflow.com/a/34994591
-template <typename _Tp, typename... _Ts> inline
-std::tuple<_Tp, _Tp, _Ts...> TransformVecs(
-      const MatrixBase<typename _Tp::value_type, _Tp::ndim, _Tp::ndim> &M,
-      const _Tp &vec0, const _Tp &vec1, const _Ts &... others) {
-  const int num_vecs = 2 + sizeof...(others);
-  MatrixBase<typename _Tp::value_type, _Tp::ndim, num_vecs> transformed;
-  transformed = M * VecsToEigen(vec0, vec1, std::forward<_Tp>(others)...); // fwd uses _Tp to ensure they're all the same type!
 
-  _Tp results[num_vecs];
-  return VecsToTuple<_Tp, _Tp, _Ts...>(std::initializer_list<_Tp>{results});
-  //return VecsToTuple<_Tp, _Ts...>();//FIXMEtransformed);
+template <typename Array, std::size_t... Idx>
+auto ArrayToTuple(const Array &arr, std::index_sequence<Idx...>) {
+  return std::make_tuple(arr[Idx]...);
+}
+
+
+template <typename _Tp, int Rows, int Columns>
+auto EigenMatToVecTuple(
+    const Matrix<_Tp, Rows, Columns> &vec_mat) {
+  std::array<Vec<_Tp, Rows>, Columns> arr;
+  for (int c = 0; c < Columns; ++c) {
+    arr[c] = EigenColToVec<Vec<_Tp, Rows>, Columns>(vec_mat, c);//vec_mat.col(c));
+  }
+  return ArrayToTuple(arr, std::make_index_sequence<Columns>{});
+}
+
+
+///// Applies the `Rows x Dim` transformation matrix on the given Dim-dimensional
+///// vector, *i.e.* returns `mat * vec`, which is a Rows-dimensional vector.
+//template <typename _V, int Rows> inline
+//Vec<typename _V::value_type, Rows>
+//Transform(
+//    const Matrix<typename _V::value_type, Rows, _V::ndim> &mat,
+//    const _V &vec) {
+//  const VectorBase<
+//      typename _V::value_type, _V::ndim> res = mat * VecToEigenCol(vec);
+//  return EigenColToVec(res);
+//}
+
+
+/// Computes `mat * [vec0, ...]` and returns the result as a matrix.
+template <typename _V, typename... _Vs, int Rows> inline
+Matrix<typename _V::value_type, Rows, 1 + sizeof...(_Vs)>
+TransformToMat(
+      const Matrix<typename _V::value_type, Rows, _V::ndim> &mat,
+      const _V &vec0, const _Vs &... others) {
+  return mat * VecsToEigenMat<_V::ndim>(vec0, others...);
+}
+
+
+/// Computes `mat * [vec0, vec1, ...]` and returns a tuple of vectors.
+///
+/// Example:
+///   >>> wkg::Vec2d v1{17, 42}, v2{9, -3}, v3{0, 0.01};
+///   >>> wkg::Matrix<double, 4, 2> M;
+///   >>> M << 1, 2, 3, 4,
+///   >>>      5, 6, 7, 8;
+///   >>> wkg::Vec4d a, b, c;
+///   >>> std::tie(out1, out2, out3) = TransformToVecs(
+///   >>>   T_matrix, in1, in2, in3);
+template <typename _V, typename... _Vs, int Rows> inline
+std::tuple<
+  Vec<typename _V::value_type, Rows>,
+  Vec<typename _Vs::value_type, Rows>...>
+TransformToVecs(
+      const Matrix<typename _V::value_type, Rows, _V::ndim> &mat,
+      const _V &vec0, const _Vs &... others) {
+
+  constexpr int num_vecs = 1 + sizeof...(others);
+  const auto transformed = TransformToMat(mat, vec0, others...);
+  return EigenMatToVecTuple<typename _V::value_type, Rows, num_vecs>(transformed);
+}
+
+
+
+
+//template <typename _V, typename... _Vs, int Rows> inline
+////Matrix<typename _V::value_type, Rows, 2 + sizeof...(_Vs)>
+//////Matrix<typename _V::value_type, Rows, Eigen::Dynamic>
+//auto ProjectToMat(
+//      const Matrix<typename _V::value_type, Rows, _V::ndim> &mat,
+//      const _V &vec0, const _V &vec1, const _Vs &... others) {
+
+//  auto transformed = TransformToMat(mat, vec0, vec1, others...);
+//  return transformed.colwise().hnormalized();
+//}
+
+
+//template <typename _V, typename... _Vs, int Rows, int Columns> inline
+////Matrix<typename _V::value_type, Rows, 2 + sizeof...(_Vs)>
+//////Matrix<typename _V::value_type, Rows, Eigen::Dynamic>
+//auto ProjectInhomogeneousToMat(
+//    const Matrix<typename _V::value_type, Rows, Columns> &mat,
+//    const _V &vec0, const _V &vec1, const _Vs &... others) {
+
+//  static_assert(
+//    Columns == _V::ndim + 1,
+//    "Number of columns in projection matrix must be _V::ndim + 1!");
+
+//  auto transformed = mat * VecsToEigenMat<Columns, _V, _Vs...>(vec0, vec1, others...);
+////  auto transformed = VecsToEigenMat<Columns>(vec0, vec1, others...);
+//  return transformed.colwise().hnormalized();
+////  return transformed;
+//}
+
+
+
+template <typename _V, typename... _Vs, int Rows> inline
+Matrix<typename _V::value_type, Rows, 1 + sizeof...(_Vs)>
+////Matrix<typename _V::value_type, Rows, Eigen::Dynamic>
+//auto
+ProjectInhomogeneousToMat(
+      const Matrix<typename _V::value_type, Rows, _V::ndim + 1> &mat,
+      const _V &vec0, const _Vs &... others) {
+
+  constexpr int num_vecs = 1 + sizeof...(others);
+
+  const Matrix<typename _V::value_type, Rows, num_vecs> transformed =
+      mat * VecsToEigenMat<_V::ndim + 1>(vec0, others...);
+
+  return transformed.colwise().hnormalized();
+}
+
+
+template <typename _V, typename... _Vs, int Rows> inline
+std::tuple<
+  Vec<typename _V::value_type, Rows - 1>,
+  Vec<typename _Vs::value_type, Rows - 1>...>
+ProjectInhomogeneousToVecs(
+      const Matrix<typename _V::value_type, Rows, _V::ndim + 1> &mat,
+      const _V &vec0, const _Vs &... others) {
+
+  constexpr int num_vecs = 1 + sizeof...(others);
+
+//  const auto hnormed = ProjectInhomogeneousToMat(mat, vec0, others...);
+
+////  const Matrix<typename _V::value_type, Rows, num_vecs> transformed =
+////      TransformToMat(mat, vec0, vec1, others...);
+  const Matrix<typename _V::value_type, Rows, num_vecs> transformed =
+      mat * VecsToEigenMat<_V::ndim + 1>(vec0, others...);
+
+  const auto hnormed = transformed.colwise().hnormalized();
+  return EigenMatToVecTuple<typename _V::value_type, Rows - 1, num_vecs>(hnormed);
 }
 
 } // namespace geometry
