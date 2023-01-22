e92dafb25735ce2ea8028c7e94f06a473c96a15a
snototter <snototter@users.noreply.github.com>
2023-01-22 12:51:53 +0100

    update ci action

--- cmake/project-is-top-level.cmake
+++ cmake/project-is-top-level.cmake
@@ -1,6 +1,9 @@
 # This variable is set by project() in CMake 3.21+
-string(
-    COMPARE EQUAL
-    "${CMAKE_SOURCE_DIR}" "${PROJECT_SOURCE_DIR}"
-    PROJECT_IS_TOP_LEVEL
-)
+if(CMAKE_VERSION VERSION_LESS 3.21.0)
+    string(
+        COMPARE EQUAL
+        "${CMAKE_SOURCE_DIR}" "${PROJECT_SOURCE_DIR}"
+        PROJECT_IS_TOP_LEVEL
+    )
+endif()
+
