ad294823f630820299640bea0049caac70f85567
snototter <snototter@users.noreply.github.com>
2023-01-22 11:44:32 +0100

    fix repo url

--- cmake/werkzeugkiste_dependencies.cmake
+++ cmake/werkzeugkiste_dependencies.cmake
@@ -1,18 +1,18 @@
 function(setup_dependency_eigen)
     # Checks if Eigen is installed. If not, it will be fetched and linked
-    find_package(Eigen3 QUIET)
-    if(Eigen3_FOUND)
-        message(STATUS "[werkzeugkiste] Using locally available Eigen.")
-    else()
+    find_package(Eigen3 3.3 QUIET NO_MODULE)
+    if (TARGET Eigen3::Eigen)
+        message(STATUS "[werkzeugkiste] Using locally available Eigen3.")
+    else(TARGET Eigen3::Eigen)
         # Fetch the library:
-        message(STATUS "[werkzeugkiste] Including Eigen via FetchContent.")
+        message(STATUS "[werkzeugkiste] Including Eigen3 via FetchContent.")
         include(FetchContent)
         FetchContent_Declare(
-            Eigen3
-            https://gitlab.com/libeigen/eigen.git
-            GIT_TAG refactor-cmake-init
-            GIT_TAG master)
-        FetchContent_MakeAvailable(Eigen3)
-    endif()
+          Eigen
+          GIT_REPOSITORY  https://gitlab.com/libeigen/eigen.git
+          GIT_TAG         master
+        )
+        FetchContent_MakeAvailable(Eigen)
+    endif(TARGET Eigen3::Eigen)
 endfunction()
 
