c9e4e3268a63678d67e910f4abda1d580a09b5fd
snototter <snototter@users.noreply.github.com>
2023-01-22 13:00:24 +0100

    fix deprecated non-lambda usage

--- src/strings/strings.cpp
+++ src/strings/strings.cpp
@@ -98,7 +98,7 @@ std::string LTrim(std::string_view totrim)
   s.erase(
       s.begin(),
       std::find_if(
-          s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+          s.begin(), s.end(), [](int c) { return !std::isspace(c); }));
   return s;
 }
 
@@ -107,7 +107,7 @@ std::string RTrim(std::string_view totrim)
   std::string s(totrim);
   s.erase(
       std::find_if(
-          s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace)))
+          s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); })
           .base(),
       s.end());
   return s;
