5bb0d2a9f2dba794e3cc15f7227bab9923929afb
snototter <snototter@users.noreply.github.com>
2023-02-11 01:22:30 +0100

    linted

--- src/config/keymatcher.cpp
+++ src/config/keymatcher.cpp
@@ -45,14 +45,14 @@ struct KeyMatcher::Impl {
  private:
   std::vector<std::pair<std::string, std::optional<std::regex>>> patterns_{};
 
-  inline bool IsRegex(std::string_view key) {
+  static bool IsRegex(std::string_view key) {
     const auto *it = std::find_if_not(key.begin(), key.end(), [](char c) {
       return ((isalnum(c) != 0) || (c == '.') || (c == '_') || (c == '-'));
     });
     return it != key.end();
   }
 
-  inline std::optional<std::regex> BuildRegex(std::string_view key) {
+  static std::optional<std::regex> BuildRegex(std::string_view key) {
     if (!IsRegex(key)) {
       return std::nullopt;
     }
