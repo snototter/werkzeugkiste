1b582d555c905ae4d654ea9813239a87df1ef5d6
snototter <snototter@users.noreply.github.com>
2023-03-14 22:39:53 +0100

    wip: try fetching nl::json

---
+++ src/config/json.cpp
@@ -0,0 +1,14 @@
+#include <werkzeugkiste/config/configuration.h>
+#include <werkzeugkiste/files/fileio.h>
+#include <werkzeugkiste/logging.h>
+#include <werkzeugkiste/strings/strings.h>
+
+#include <cstdint>
+#include <iomanip>
+#include <limits>
+#include <nlohmann/json.hpp>
+#include <sstream>
+#include <string_view>
+#include <type_traits>
+
+namespace werkzeugkiste::config {}  // namespace werkzeugkiste::config
