e0e92cf13a43aa2316b07573eb1f8455c0edc62f
snototter <snototter@users.noreply.github.com>
2023-03-15 22:00:49 +0100

    remove unused include

--- src/config/libconfig.cpp
+++ src/config/libconfig.cpp
@@ -1,6 +1,5 @@
 #include <werkzeugkiste/config/configuration.h>
 #include <werkzeugkiste/files/fileio.h>
-#include <werkzeugkiste/logging.h>
 #include <werkzeugkiste/strings/strings.h>
 
 #include <cstdint>
