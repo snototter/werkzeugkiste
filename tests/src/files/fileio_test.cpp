7f5353417a6812d9d9ce7bad5cac072d34fe1d51
snototter <snototter@users.noreply.github.com>
2023-03-25 00:14:49 +0100

    add missing tests

--- tests/src/files/fileio_test.cpp
+++ tests/src/files/fileio_test.cpp
@@ -17,22 +17,22 @@ TEST(FileIOTest, ReadFile) {
   auto concatenated = wks::RTrim(wks::Concatenate(lines, "\n"));
   EXPECT_EQ(content.length(), concatenated.length());
   EXPECT_EQ(content, concatenated);
-
-  // This is a dummy comment to ensure the file ends at 42 lines.
-  // This makes for a nice magic number...
   EXPECT_EQ(42, lines.size());
 }
 
 TEST(FileIOTest, Iterator) {
   EXPECT_THROW(wkf::AsciiFileIterator("no-such-file"), wkf::IOError);
 
+  std::size_t line_nr = 0;
   std::vector<std::string> lines;
   wkf::AsciiFileIterator iterator{__FILE__};
   while (iterator.HasLine()) {
     lines.push_back(*iterator);
+    EXPECT_EQ(line_nr, iterator.LineNumber());
+    EXPECT_EQ(0, iterator->compare(lines[lines.size() - 1]));
     ++iterator;
+    ++line_nr;
   }
-
   EXPECT_EQ(42, lines.size());
 
   std::string content = wks::RTrim(wkf::CatAsciiFile(__FILE__));
